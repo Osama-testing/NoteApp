c5a27b43aa8305d7492c72654380b63b476fd7f9
autoislamabad@gmail.com <autoislamabad@gmail.com>
2021-09-18 17:22:44 +0500

    Updated

--- NoteApp/Models/ListViewModel.cs
+++ NoteApp/Models/ListViewModel.cs
@@ -7,16 +7,12 @@ namespace NoteApp.Models
 {
     public class ListViewModel
     {
-        //public IEnumerable<ListModel> ListModels { get; set; }
-        //public IEnumerable<NotesModel> NotesModel { get; set; }
-        //public IEnumerable<TagModel> TagModel { get; set; }
         public ListModel ListModels { get; set; }
         public NotesModel NotesModel { get; set; }
         public TagModel TagModel { get; set; }
 
         public HttpPostedFileBase File { get; set; }
         public List<string> Tags { get; set; }
-        //  public IEnumerable<ListModel> ListModel { get; set; }
         public List<ListModel> GetListModels { get; set; }
         public List<NotesModel> GetNotesModels { get; set; }
         public List<NotesModel> GetNotes { set; get; }
